bf149c079312001ef06f7e4019db34ce0762d6c4
Siva Dinesh k <ksd.ccbp4@gmail.com>
2024-03-07 12:39:20 +0000

    all functaniolaties added

--- src/App.js
+++ src/App.js
@@ -17,11 +17,62 @@ class App extends Component {
     cartList: [],
   }
 
-  //   TODO: Add your code for remove all cart items, increment cart item quantity, decrement cart item quantity, remove cart item
+  //  code to remove all cart items, increment cart item quantity, decrement cart item quantity, remove cart item
 
   addCartItem = product => {
-    this.setState(prevState => ({cartList: [...prevState.cartList, product]}))
+    const {cartList} = this.state
+
+    if (cartList.find(item => item.id === product.id)) {
+      this.setState(prevState => ({
+        cartList: prevState.cartList.map(item => {
+          if (item.id === product.id) {
+            return {...item, quantity: item.quantity + product.quantity}
+          }
+
+          return item
+        }),
+      }))
+    } else {
+      this.setState(prevState => ({cartList: [...prevState.cartList, product]}))
+    }
     //   TODO: Update the code here to implement addCartItem
+  }
+
+  removeCartItem = id => {
+    const {cartList} = this.state
+
+    const newList = cartList.filter(item => item.id !== id)
+
+    this.setState({cartList: newList})
+  }
+
+  incrementCartItemQuantity = id => {
+    this.setState(prevState => ({
+      cartList: prevState.cartList.map(item => {
+        if (item.id === id) {
+          return {...item, quantity: item.quantity + 1}
+        }
+        return item
+      }),
+    }))
+  }
+
+  decrementCartItemQuantity = id => {
+    const {cartList} = this.state
+    const product = cartList.find(item => item.id === id)
+
+    if (product.quantity > 1) {
+      this.setState(prevState => ({
+        cartList: prevState.cartList.map(item => {
+          if (item.id === id) {
+            return {...item, quantity: item.quantity - 1}
+          }
+          return item
+        }),
+      }))
+    } else {
+      this.removeCartItem(id)
+    }
   }
 
   render() {
@@ -33,6 +84,8 @@ class App extends Component {
           cartList,
           addCartItem: this.addCartItem,
           removeCartItem: this.removeCartItem,
+          incrementCartItemQuantity: this.incrementCartItemQuantity,
+          decrementCartItemQuantity: this.decrementCartItemQuantity,
         }}
       >
         <Switch>
